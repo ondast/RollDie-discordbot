4e8c2478e1bc283ec6083a572aede7fc4a061685
Marcus Svedberg <ondast@gmail.com>
2022-02-04 10:21:59 +0100

    Command deployment outline and roll command

---
+++ commands/roll.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const Service = require('../utils/service.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roll')
+		.setDescription('Rolls one or a number of die. Syntax: 1d6, d6, 2d6+1')
+        .addStringOption(option =>
+            option.setName('combination')
+                .setDescription('Syntax: 1d6, d6, 2d6+1')
+                .setRequired(false)),
+
+	async execute(interaction) {
+        let embed = null;
+        let mess = '';
+
+        try {
+            let res = Service.parse(interaction.options.getString('combination'));
+            embed = Service.getQuote();
+            mess = Service.generateMessage(res, interaction.user.username);
+        } catch(error) {
+            mess = 'Nope';
+        }
+
+        if (embed)
+            await interaction.reply({ content: mess, embeds: [embed] });
+        else
+            await interaction.reply({ content: mess });
+
+  	},
+};
