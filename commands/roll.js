45719f64b37d0b92e16099bd288d7641965c4b1a
Marcus Svedberg <ondast@gmail.com>
2022-02-04 10:47:07 +0100

    Fix tabs/spaces indentation.

--- commands/roll.js
+++ commands/roll.js
@@ -3,15 +3,15 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const Service = require('../utils/service.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('roll')
-		.setDescription('Rolls one or a number of die with a number of modifiers. Syntax: 1d6, d6, 2d6+1')
+    data: new SlashCommandBuilder()
+        .setName('roll')
+        .setDescription('Rolls one or a number of die with a number of modifiers. Syntax: 1d6, d6, 2d6+1')
         .addStringOption(option =>
             option.setName('combination')
                 .setDescription('Syntax: 1d6, d6, 2d6+1, 2d6 + 1d6 + 3 - 1')
                 .setRequired(false)),
 
-	async execute(interaction) {
+    async execute(interaction) {
         let embed = null;
         let mess = '';
 
