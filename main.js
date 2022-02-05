9c53bfdfbc4c7cd8c013fa166844b0e3fc9b0fd8
Marcus Svedberg <ondast@gmail.com>
2022-02-05 15:52:25 +0100

    Cleanup and refactor

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 // External includes
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES] });
 
 // Internal includes
 const { token } = require('./config.json');
-// const Service = require('./utils/service.js');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 const services = {};
+
+// Set up client
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES] });
 client.commands = new Collection();
 
 // Parse commands and tally services
@@ -69,4 +71,5 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
+// Login
 client.login(token);
